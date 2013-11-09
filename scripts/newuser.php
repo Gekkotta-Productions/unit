badcc765a78cc82bb3d77ab5baa39f23b63e9209
Stephen Nestor <fenris1337@gmail.com>
2013-11-09 02:53:54 -0500

    newuser.php updated for both tables

--- scripts/newuser.php
+++ scripts/newuser.php
@@ -7,31 +7,13 @@ $name = $_GET["name"];
 $email = $_GET["email"];
 $teamname = $_GET["teamname"];
 
-$teamquery = "INSERT INTO Teams(`name`) VALUES($teamname)";
-$objQuery = mysql_query($teamquery);
+$teamQuery = "INSERT INTO Teams(name) VALUES('$teamname')";
+$userQuery = "INSERT INTO Players(name, email, IGname) VALUES('$name','$email','$IGname')";
 
+$teamObjQuery = mysql_query($teamQuery);
+$userObjQuery = mysql_query($userQuery);
 
-print json_encode($objQuery);
+print json_encode($teamObjQuery);
+print json_encode($userObjQuery);
 
-
-/*
-if ($id == "a"){
-	$query = "SELECT * FROM Players";
-}
-else {
-	$query = "SELECT * FROM Players WHERE `playerID` = $id";
-}
-
-$objQuery = mysql_query($query);
-
-$rows = array();
-
-while ($r = mysql_fetch_assoc($objQuery)){
-	$rows[] = $r;
-}
-
-print '{"unit":';
-print json_encode($rows);
-print '}';
-*/
 ?>

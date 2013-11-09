1e70d92f32fedd273d0bad6d701a06908b36d7a4
StanleyFung <stanleyfung94@hotmail.com>
2013-11-08 20:44:05 -0500

    BUMP Slightly Functional

--- unit/gen/com/gekkotta/productions/unit/R.java
+++ unit/gen/com/gekkotta/productions/unit/R.java
@@ -17,49 +17,50 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static int activity_horizontal_margin=0x7f040000;
-        public static int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static int bump=0x7f020000;
-        public static int ic_launcher=0x7f020001;
-        public static int name=0x7f020002;
+        public static final int bump=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int name=0x7f020002;
     }
     public static final class id {
-        public static int action_settings=0x7f08000b;
-        public static int b_bump=0x7f080001;
-        public static int b_click=0x7f080002;
-        public static int cb_status=0x7f08000a;
-        public static int iv_bump=0x7f080006;
-        public static int name=0x7f080004;
-        public static int tv_instructions=0x7f080007;
-        public static int tv_result=0x7f080009;
-        public static int tv_score=0x7f080003;
-        public static int tv_status=0x7f080008;
-        public static int tv_team_name=0x7f080000;
-        public static int tv_title=0x7f080005;
+        public static final int action_settings=0x7f08000c;
+        public static final int b_BUMP=0x7f080005;
+        public static final int b_bump=0x7f080001;
+        public static final int b_click=0x7f080002;
+        public static final int cb_status=0x7f08000b;
+        public static final int iv_bump=0x7f080007;
+        public static final int name=0x7f080004;
+        public static final int tv_instructions=0x7f080008;
+        public static final int tv_result=0x7f08000a;
+        public static final int tv_score=0x7f080003;
+        public static final int tv_status=0x7f080009;
+        public static final int tv_team_name=0x7f080000;
+        public static final int tv_title=0x7f080006;
     }
     public static final class layout {
-        public static int activity_achievement=0x7f030000;
-        public static int activity_clicking=0x7f030001;
-        public static int activity_main=0x7f030002;
-        public static int activity_upgrade=0x7f030003;
-        public static int bump=0x7f030004;
-        public static int team_score=0x7f030005;
-        public static int tutorial=0x7f030006;
+        public static final int activity_achievement=0x7f030000;
+        public static final int activity_clicking=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_upgrade=0x7f030003;
+        public static final int bump=0x7f030004;
+        public static final int team_score=0x7f030005;
+        public static final int tutorial=0x7f030006;
     }
     public static final class menu {
-        public static int main=0x7f070000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static int action_settings=0x7f050001;
-        public static int app_name=0x7f050000;
-        public static int bump_button=0x7f050005;
-        public static int click_button=0x7f050006;
-        public static int hello_world=0x7f050002;
-        public static int name=0x7f050003;
-        public static int player_score=0x7f050007;
-        public static int team_name=0x7f050004;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int bump_button=0x7f050005;
+        public static final int click_button=0x7f050006;
+        public static final int hello_world=0x7f050002;
+        public static final int name=0x7f050003;
+        public static final int player_score=0x7f050007;
+        public static final int team_name=0x7f050004;
     }
     public static final class style {
         /** 
@@ -83,10 +84,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f060001;
     }
 }

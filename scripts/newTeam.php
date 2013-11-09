cef250c32dd1af04179d90187584b88f67b4daae
Stephen Nestor <fenris1337@gmail.com>
2013-11-09 04:28:05 -0500

    Uploaded newTeam script

--- scripts/newTeam.php
+++ scripts/newTeam.php
@@ -5,10 +5,18 @@ $objDB = mysql_select_db("unit");
 $teamname = $_GET["teamname"];
 
 $teamQuery = "SELECT name FROM Teams";
+$result = mysql_query($teamQuery);
+while($r=mysql_fetch_assoc($result)){
+	$uniqueTeam=  in_array($teamname, $r);
+	if($uniqueTeam == true){
+	print FALSE;
+	exit
+	}
+}
 
 $teamInput = "INSERT INTO Teams(name) VALUES('$teamname')";
 
-$teamObjQuery = mysql_query($teamQuery);
+$teamObjQuery = mysql_query($teamInput);
 
 print json_encode($teamObjQuery);
 ?>

1a9bfa23e06f704e656495e30f976d5322526ce2
Stephen Nestor <fenris1337@gmail.com>
2013-11-09 01:46:01 -0500

    New user function added

---
+++ scripts/newuser.php
@@ -0,0 +1,33 @@
+<?php
+$objConnect = mysql_connect("localhost","root","");
+$objDB = mysql_select_db("unit");
+
+$teamname = $_GET["teamname"];
+$query = "INSERT INTO Teams($teamname)";
+$objQuery = mysql_query($query);
+print '{"unit":';
+print json_encode($objQuery);
+print '}';
+
+
+/*
+if ($id == "a"){
+	$query = "SELECT * FROM Players";
+}
+else {
+	$query = "SELECT * FROM Players WHERE `playerID` = $id";
+}
+
+$objQuery = mysql_query($query);
+
+$rows = array();
+
+while ($r = mysql_fetch_assoc($objQuery)){
+	$rows[] = $r;
+}
+
+print '{"unit":';
+print json_encode($rows);
+print '}';
+*/
+?>
